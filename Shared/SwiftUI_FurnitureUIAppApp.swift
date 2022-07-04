9fc336311bcd01d891ca81d952f2291fc87d618d
boardguy1024 <boardguy1024@gmail.com>
2022-07-04 22:28:30 +0900

    Initial Commit

---
+++ Shared/SwiftUI_FurnitureUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FurnitureUIAppApp.swift
+//  Shared
+//
+//  Created by park kyung seok on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FurnitureUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
